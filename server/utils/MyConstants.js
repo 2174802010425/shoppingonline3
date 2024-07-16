2fb42afb6173ea111092aa070da642658375328f
quang huy <127500798+2174802010425@users.noreply.github.com>
2024-07-16 11:24:29 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clusterh.waaupyx.mongodb.net',
+    DB_USER: 'quanghuy',
+    DB_PASS: 'huydeptrai123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '0307',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'huyzed2003@gmail.com', // gmail service
+    EMAIL_PASS: 'vlhr iyuo jhxn ukpe'
+  };
+module.exports = MyConstants;
